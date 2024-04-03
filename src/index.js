02f4d13cca6c2e1c27976f773abf7167f9e682b3
Jiang Shunjia <ustcjsj@ustc.edu>
2024-04-03 17:38:24 +0800

    项目结构梳理

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
+//项目入口
+
+// core package
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+//导入项目的根组件
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//把App根组件渲染到id为root的dom节点上
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
