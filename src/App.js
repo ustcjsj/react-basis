936f4b1fc2876ef7033ddf7fa90d1d92f98c56c6
Jiang Shunjia <ustcjsj@ustc.edu>
2024-04-03 21:27:22 +0800

    使用tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 //项目的根组件
 // App ->index.js ->public/index.html(root)
+import { useState } from 'react';
+import  './index.css'
 
 function App() {
   const message = 'this is a message'
@@ -9,9 +11,7 @@ function App() {
     { id: 1003, name: 'Angular' }
   ];
   const flag = true;
-  let clickHandler = (name,e) => {
-    console.log('点击按钮', name,e);
-  }
+
   return (
     <div className="App">
       {message}
@@ -21,10 +21,35 @@ function App() {
 
       {flag && <span>hi</span>}
       {flag ? <span>hi</span> : null}
-      <button onClick={(e)=>clickHandler(message,e)}>click me</button>
+      <Button></Button>
+      <TestState></TestState>
+
+
+      <div className="bg-blue-500 text-white p-4">
+        This is a Tailwind CSS styled component.
+      </div>
     </div>
 
   );
+}
+const Button = () => {
+  const clickHandler = (name, e) => {
+    console.log('点击按钮', name, e);
+  }
+  return <button onClick={(e) => clickHandler("jack", e)}>click me</button>
+
+}
+const TestState = () => {
+  // const [form, setform] = useState({ name: 'jack' })
+  // return <button onClick={() => setform({
+  //   ...form,
+  //   name: 'john'
+  // })}>修改form {form.name}</button>
+  const handleClick = () => {
+    setList([5, 6, 7, 8, 9]);
+  }
+  const [list, setList] = useState([1, 2, 3, 4, 5])
+  return <button onClick={handleClick}>{list}</button>
 }
 
 export default App;
